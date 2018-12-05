b3ad5ef38065b9df93c4a50306d4c992a813b454
Simon Annetts <simona@ateb.co.uk>
2018-12-05 14:06:02 +0000

    Bug fixes

--- get_shapefiles.sh
+++ get_shapefiles.sh
@@ -9,10 +9,14 @@ echo "Downloading shapefiles..."
 
 [ ! -d mapnik/data ] && mkdir mapnik/data
 cd mapnik/data
-wget http://data.openstreetmapdata.com/water-polygons-generalized-3857.zip
-wget http://data.openstreetmapdata.com/water-polygons-split-3857.zip
+rm -f water-polygons-generalized-3857.zip*
+rm -f water-polygons-split-3857.zip*
+wget --progress=dot:giga -O water-polygons-generalized-3857.zip http://data.openstreetmapdata.com/water-polygons-generalized-3857.zip
+wget --progress=dot:giga -O water-polygons-split-3857.zip http://data.openstreetmapdata.com/water-polygons-split-3857.zip
 unzip -o water-polygons-generalized-3857.zip
+rm -f water-polygons-generalized-3857.zip
 unzip -o water-polygons-split-3857.zip
+rm -f water-polygons-split-3857.zip
 
 endtime=$(date +%s)
 echo "Done in $[${endtime}-${starttime}] seconds! All your Maps Belong to Us!"
