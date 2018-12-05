0967db590c7de7b2218255501e67d4f089904184
Simon Annetts <simona@ateb.co.uk>
2018-12-05 13:53:35 +0000

    Script to download the shapefiles we need

---
+++ get_shapefiles.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+exec > >(awk '{print strftime("%Y-%m-%d %H:%M:%S [1] "),$0; fflush();}')
+exec 2> >(awk '{print strftime("%Y-%m-%d %H:%M:%S [2] "),$0; fflush();}' >&2)
+
+starttime=$(date +%s)
+
+echo "Downloading shapefiles..."
+
+[ ! -d mapnik/data ] && mkdir mapnik/data
+cd mapnik/data
+wget http://data.openstreetmapdata.com/water-polygons-generalized-3857.zip
+wget http://data.openstreetmapdata.com/water-polygons-split-3857.zip
+unzip -o water-polygons-generalized-3857.zip
+unzip -o water-polygons-split-3857.zip
+
+endtime=$(date +%s)
+echo "Done in $[${endtime}-${starttime}] seconds! All your Maps Belong to Us!"
